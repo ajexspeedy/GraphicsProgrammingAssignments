1a656223a1f9e537bd1ad1174ed41ae13d9edf00
ajexspeedy <tim.vermant@outlook.com>
2021-02-09 14:19:02 +0100

    Start

---
+++ Week1/SceneSelector.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include "SceneSelector.h"
+#include "SceneManager.h"
+
+//Change this define to activate/deactive the corresponding scenes
+// W01 - W02  (#define ...)
+
+#define W01
+#define W02
+
+#ifdef W01
+#include "TestScene.h"
+#include "PhysXTestScene.h"
+#include "BouncingBallScene.h"
+#include "FrictionTestScene.h"
+
+#endif
+
+#ifdef W02
+
+#endif
+
+namespace dae
+{
+	void AddScenes(SceneManager * pSceneManager)
+	{
+
+#ifdef W01
+		pSceneManager->AddGameScene(new FrictionTestScene());
+		pSceneManager->AddGameScene(new BouncingBallScene());
+		pSceneManager->AddGameScene(new TestScene());
+		pSceneManager->AddGameScene(new PhysXTestScene());
+	
+#endif
+
+#ifdef W02
+
+#endif
+
+	}
+}
+
